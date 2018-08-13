99c5eceda1a95e0c1e090959b3ec088ea5b117e9
Joyce Lee <hjl435@nyu.edu>
2018-08-13 00:43:56 -0700

    Initial commit. Added homework1 and project folders. Also added homework1.html, homework1.css, and homework1.js files.

---
+++ class1/class1.js
@@ -0,0 +1,19 @@
+//Create a function that accepts 2 arguments and multiplies them together and returns the result
+
+function myMultiplyer(argument1, argument2) {
+  return argument1 * argument2;
+}
+
+let mybool = true;
+//if the req in the parentheses is true, then the console log will show the following; same with the else
+//not equal can be noted by !==
+if (mybool !== true) {
+  console.log("True");
+}
+else {
+  console.log("False");
+}
+
+//strings are defined by "_"
+//Tripe "=" tests for the type as well, not just the character
+//To negate any value, just put the ! in front of it 
